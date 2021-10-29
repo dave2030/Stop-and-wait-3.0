15975aabc627643551e6f4ac0abf35c5318583ae
Shyam Dave <52425817+dave2030@users.noreply.github.com>
2021-10-29 19:22:41 -0400

    Changes to fe

--- GUI.java
+++ GUI.java
@@ -1,173 +1,168 @@
-import java.awt.BorderLayout;
+import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
+
 public class GUI extends JFrame {
-    private final Handler handler;
+    private final Handler hand;
 
     public GUI() {
-        setTitle("CP372 A02");
-        handler = new Handler();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 375, 450);
+        hand = new Handler();
+        setTitle("Stop and Wait 3.0");
+        setBounds(200, 200, 400, 400);
         setResizable(false);
-        initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        components();
     }
 
-    private void btnReceiveHandler(ActionEvent e) {
-        // Check if we are currently receiving or not based on button
-        if (buttonReceive.getText().equals("RECEIVE")) {
+    private void bReceiveHandler(ActionEvent e) {
+        if (bReceive.getText().equals("RECEIVE")) {
             try {
-                int receiverPort = Integer.parseInt(txtReceiverPort.getText());
-                int senderPort = Integer.parseInt(txtSenderPort.getText());
-                String address = txtSenderAddress.getText();
-                String outputFileName = txtOutputFileName.getText();
+                int rPort = Integer.parseInt(tReceiverPort.getText());
+                int sPort = Integer.parseInt(tSendPort.getText());
+                String addy = tSendAddy.getText();
+                String out = tOutFile.getText();
 
 
-                // Background process to start receiving so we don't block our GUI and its repainting
                 new SwingWorker<Void, Void>() {
                     @Override
                     public Void doInBackground() {
                         try {
-                            handler.setInOrderPacketLabel(lblPacketsReceived); // Assign our JLabel to our ReceiverHandler to update it
-                            handler.startReceiving(address, senderPort, receiverPort, outputFileName, true);
+                            hand.setInOrderPacketLabel(lPacketReceive);
+                            hand.startReceiving(addy, sPort, rPort, out, true);
                         } catch (IOException exception) {
                             exception.printStackTrace();
                         }
                         return null;
                     }
                 }.execute();
-                buttonReceive.setText("TERMINATE");
+                bReceive.setText("TERMINATE");
             } catch (NumberFormatException exception) {
                 JOptionPane.showMessageDialog(this, "Invalid port number(s), please enter a number", "Error", JOptionPane.ERROR_MESSAGE);
             }
         } else {
-            ///disconnect
-            handler.stopReceiving();
-            buttonReceive.setText("RECEIVE");
+            hand.stopReceiving();
+            bReceive.setText("RECEIVE");
         }
     }
 
-    private void initComponents() {
-        panelParent = new JPanel();
-        panelParent.setBorder(new EmptyBorder(5, 5, 5, 5));
-        panelParent.setLayout(new BorderLayout(0, 0));
-        setContentPane(panelParent);
+    private void components() {
+        par = new JPanel();
+        par.setBorder(new EmptyBorder(50, 50, 50, 50));
 
-        panelHeader = new JPanel();
-        panelParent.add(panelHeader, BorderLayout.NORTH);
-        panelHeader.setLayout(new BorderLayout(0, 0));
-        
-
-        panelMain = new JPanel();
-        panelParent.add(panelMain, BorderLayout.CENTER);
-        panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.Y_AXIS));
-
-        panelSenderAddress = new JPanel();
-        panelSenderAddress.setBorder(new EmptyBorder(10, 15, 10, 15));
-        panelMain.add(panelSenderAddress);
-        panelSenderAddress.setLayout(new BorderLayout(0, 0));
-
-        lblSenderAddress = new JLabel("IP Address of Receiver:");
-        lblSenderAddress.setBorder(new EmptyBorder(0, 0, 0, 0));
-        panelSenderAddress.add(lblSenderAddress, BorderLayout.WEST);
-
-        txtSenderAddress = new JTextField();
-        txtSenderAddress.setText("127.0.0.1");
-        panelSenderAddress.add(txtSenderAddress, BorderLayout.SOUTH);
-        txtSenderAddress.setColumns(10);
-
-        panelSenderPort = new JPanel();
-        panelSenderPort.setBorder(new EmptyBorder(5, 15, 5, 15));
-        panelMain.add(panelSenderPort);
-        panelSenderPort.setLayout(new BorderLayout(0, 0));
-
-        lblSenderPort = new JLabel("UDP Sender Port:");
-        lblSenderPort.setBorder(new EmptyBorder(0, 0, 0, 0));
-        panelSenderPort.add(lblSenderPort, BorderLayout.WEST);
-
-        txtSenderPort = new JTextField();
-        txtSenderPort.setText("3321");
-        txtSenderPort.setColumns(10);
-        panelSenderPort.add(txtSenderPort, BorderLayout.SOUTH);
-
-        panelReceiverPort = new JPanel();
-        panelReceiverPort.setBorder(new EmptyBorder(5, 15, 5, 15));
-        panelMain.add(panelReceiverPort);
-        panelReceiverPort.setLayout(new BorderLayout(0, 0));
-
-        lblReceiverPort = new JLabel("UDP Receiver Port:");
-        lblReceiverPort.setBorder(new EmptyBorder(0, 0, 0, 0));
-        panelReceiverPort.add(lblReceiverPort, BorderLayout.WEST);
-
-        txtReceiverPort = new JTextField();
-        txtReceiverPort.setText("4455");
-        txtReceiverPort.setColumns(10);
-        panelReceiverPort.add(txtReceiverPort, BorderLayout.SOUTH);
-
-        panelOutputFileName = new JPanel();
-        panelOutputFileName.setBorder(new EmptyBorder(5, 15, 20, 15));
-        panelMain.add(panelOutputFileName);
-        panelOutputFileName.setLayout(new BorderLayout(0, 0));
-
-        lblOutputFileName = new JLabel("Output File To Be Transferred:");
-        lblOutputFileName.setBorder(new EmptyBorder(0, 0, 0, 0));
-        panelOutputFileName.add(lblOutputFileName, BorderLayout.WEST);
-
-        txtOutputFileName = new JTextField();
-        txtOutputFileName.setText("received.txt"); //TODO: REMOVE DEFAULT
-        txtOutputFileName.setColumns(10);
-        panelOutputFileName.add(txtOutputFileName, BorderLayout.SOUTH);
-
-        panelOtherComponents = new JPanel();
-        panelOtherComponents.setBorder(new EmptyBorder(10, 20, 10, 20));
-        panelMain.add(panelOtherComponents);
-        panelOtherComponents.setLayout(new BorderLayout(0, 0));
+        setContentPane(par);
+        head = new JPanel();
+        par.add(head, BorderLayout.NORTH);
+        par.setBackground(Color.BLACK);
+        head.setLayout(new BorderLayout(0, 0));
 
 
+        pMain = new JPanel();
+        par.add(pMain, BorderLayout.CENTER);
+        pMain.setLayout(new BoxLayout(pMain, BoxLayout.Y_AXIS));
 
-        buttonReceive = new JButton("RECEIVE");
-        buttonReceive.addActionListener(this::btnReceiveHandler);
-        panelOtherComponents.add(buttonReceive, BorderLayout.EAST);
+        pSendAddy = new JPanel();
+        pSendAddy.setBorder(new EmptyBorder(10, 15, 10, 15));
+        pMain.add(pSendAddy);
+        pSendAddy.setLayout(new BorderLayout(0, 0));
 
-        panelInfo = new JPanel();
-        panelMain.add(panelInfo);
+        lSendAddy = new JLabel("IP Address of Receiver:");
+        lSendAddy.setBorder(new EmptyBorder(0, 0, 0, 0));
+        pSendAddy.add(lSendAddy, BorderLayout.WEST);
 
-        lblInfo = new JLabel("Received in-order packets:");
-        panelInfo.add(lblInfo);
+        tSendAddy = new JTextField();
+        tSendAddy.setText("127.0.0.1");
+        pSendAddy.add(tSendAddy, BorderLayout.SOUTH);
+        tSendAddy.setColumns(10);
 
-        lblPacketsReceived = new JLabel("0");
-        panelInfo.add(lblPacketsReceived);
+        pSendPort = new JPanel();
+        pSendPort.setBorder(new EmptyBorder(5, 15, 5, 15));
+        pMain.add(pSendPort);
+        pSendPort.setLayout(new BorderLayout(0, 0));
+
+        lSendPort = new JLabel("UDP Sender Port:");
+        lSendPort.setBorder(new EmptyBorder(0, 0, 0, 0));
+        pSendPort.add(lSendPort, BorderLayout.WEST);
+
+        tSendPort = new JTextField();
+        tSendPort.setText("3321");
+        tSendPort.setColumns(10);
+        pSendPort.add(tSendPort, BorderLayout.SOUTH);
+
+        pReceiverPort = new JPanel();
+        pReceiverPort.setBorder(new EmptyBorder(5, 15, 5, 15));
+        pMain.add(pReceiverPort);
+        pReceiverPort.setLayout(new BorderLayout(0, 0));
+
+        lReceiverPort = new JLabel("UDP Receiver Port:");
+        lReceiverPort.setBorder(new EmptyBorder(0, 0, 0, 0));
+        pReceiverPort.add(lReceiverPort, BorderLayout.WEST);
+
+        tReceiverPort = new JTextField();
+        tReceiverPort.setText("4455");
+        tReceiverPort.setColumns(10);
+        pReceiverPort.add(tReceiverPort, BorderLayout.SOUTH);
+
+        pOutFile = new JPanel();
+        pOutFile.setBorder(new EmptyBorder(5, 15, 20, 15));
+        pMain.add(pOutFile);
+        pOutFile.setLayout(new BorderLayout(0, 0));
+
+        lOutFile = new JLabel("Output File To Be Transferred:");
+        lOutFile.setBorder(new EmptyBorder(0, 0, 0, 0));
+        pOutFile.add(lOutFile, BorderLayout.WEST);
+
+        tOutFile = new JTextField();
+        tOutFile.setText("received.txt"); //TODO: REMOVE DEFAULT
+        tOutFile.setColumns(10);
+        pOutFile.add(tOutFile, BorderLayout.SOUTH);
+
+        pOtherC = new JPanel();
+        pOtherC.setBorder(new EmptyBorder(10, 20, 10, 20));
+        pMain.add(pOtherC);
+        pOtherC.setLayout(new BorderLayout(0, 0));
+
+
+
+        bReceive = new JButton("RECEIVE");
+        bReceive.addActionListener(this::bReceiveHandler);
+        pOtherC.add(bReceive, BorderLayout.EAST);
+
+        pInfo = new JPanel();
+        pMain.add(pInfo);
+
+        lInfo = new JLabel("Received in-order packets:");
+        pInfo.add(lInfo);
+
+        lPacketReceive = new JLabel("0");
+        pInfo.add(lPacketReceive);
 
         setVisible(true);
     }
 
-    JPanel panelParent;
-    JPanel panelHeader;
-    JLabel lblNames;
-    JLabel lblTitle;
-    JPanel panelMain;
-    JPanel panelSenderAddress;
-    JLabel lblSenderAddress;
-    JTextField txtSenderAddress;
-    JPanel panelSenderPort;
-    JLabel lblSenderPort;
-    JTextField txtSenderPort;
-    JPanel panelReceiverPort;
-    JLabel lblReceiverPort;
-    JTextField txtReceiverPort;
-    JPanel panelOutputFileName;
-    JLabel lblOutputFileName;
-    JTextField txtOutputFileName;
-    JPanel panelOtherComponents;
-    JCheckBox checkboxReliable;
-    JButton buttonReceive;
-    JPanel panelInfo;
-    JLabel lblInfo;
-    JLabel lblPacketsReceived;
+    JPanel par;
+    JPanel head;
+    JPanel pMain;
+    JLabel lPacketReceive;
+    JPanel pSendPort;
+    JLabel lSendPort;
+    JTextField tSendPort;
+    JPanel pOutFile;
+    JLabel lOutFile;
+    JTextField tOutFile;
+    JPanel pInfo;
+    JLabel lInfo;
+    JPanel pOtherC;
+    JButton bReceive;
+    JPanel pSendAddy;
+    JLabel lSendAddy;
+    JTextField tSendAddy;
+    JPanel pReceiverPort;
+    JLabel lReceiverPort;
+    JTextField tReceiverPort;
+
 
 }
